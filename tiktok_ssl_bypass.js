ff8cb3eb57b165a0c2e1b9737ebece18e60138b5
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-04-15 19:16:50 +0600

    Refactor TikTok SSL Bypass script for clarity

--- tiktok_ssl_bypass.js
+++ tiktok_ssl_bypass.js
@@ -1,102 +1,114 @@
-// Respect goes to Eliton
+/**
+ * TikTok SSL Pinning Bypass - Frida Script
+ * Author: MUHAMMAD SHAKIB
+ * GitHub: https://github.com/0XSHAK1B
+ */
 
-function waitForModule(moduleName) {
-    return new Promise(resolve => {
-        const interval = setInterval(() => {
-            const module = Process.findModuleByName(moduleName);
-            if (module != null) {
-                clearInterval(interval);
-                resolve(module);
+const TAG = "SSLBypass";
+
+function log(msg) {
+    console.log(msg);
+    Java.perform(() => {
+        Java.use("android.util.Log").v(TAG, msg);
+    });
+}
+
+function awaitLibrary(name) {
+    return new Promise((done) => {
+        let timer = setInterval(() => {
+            let mod = Process.findModuleByName(name);
+            if (mod) {
+                clearInterval(timer);
+                done(mod);
             }
         }, 0);
     });
 }
 
-function hook_callback(callback) {
-    const f = new NativeFunction(callback, "int", ["pointer", "pointer"]);
-    Interceptor.attach(f, {
-        onLeave: function(retval) {
-            retval.replace(0)
+function patchVerifyCallback(cb) {
+    let fn = new NativeFunction(cb, "int", ["pointer", "pointer"]);
+    Interceptor.attach(fn, {
+        onLeave(ret) {
+            ret.replace(0);
         }
-    })
-}
-
-function hook_SSL_CTX_set_custom_verify(library) {
-    const functionName = "SSL_CTX_set_custom_verify"
-    try {
-        const f = Module.getExportByName(library.name, functionName);
-        const SSL_CTX_set_custom_verify = new NativeFunction(f, 'void', ['pointer', 'int', 'pointer'])
-
-        Interceptor.replace(SSL_CTX_set_custom_verify, new NativeCallback(function(ssl, mode, callback) {
-            hook_callback(callback);
-            SSL_CTX_set_custom_verify(ssl, mode, callback)
-        }, 'void', ['pointer', 'int', 'pointer']));
-
-        logger(`[*][+] Hooked function: ${functionName}`);
-    } catch (err) {
-        logger(`[*][-] Failed to hook function: ${functionName}`);
-        logger(err.toString())
-    }
-}
-
-function logger(message) {
-    console.log(message);
-    Java.perform(function() {
-        var Log = Java.use("android.util.Log");
-        Log.v("TIKTOK_SSL_PINNING_BYPASS", message);
     });
 }
 
-logger("[*][*] Waiting for libttboringssl...");
-waitForModule("libttboringssl.so").then((lib) => {
-    logger(`[*][+] Found libttboringssl at: ${lib.base}`)
-    hook_SSL_CTX_set_custom_verify(lib);
+function bypassCustomVerify(mod) {
+    let sym = "SSL_CTX_set_custom_verify";
+    try {
+        let addr = Module.getExportByName(mod.name, sym);
+        let original = new NativeFunction(addr, "void", ["pointer", "int", "pointer"]);
+
+        Interceptor.replace(original, new NativeCallback((ctx, mode, cb) => {
+            patchVerifyCallback(cb);
+            original(ctx, mode, cb);
+        }, "void", ["pointer", "int", "pointer"]));
+
+        log(`[+] ${sym} hooked in ${mod.name}`);
+    } catch (e) {
+        log(`[-] ${sym} failed in ${mod.name}: ${e}`);
+    }
+}
+
+// --- Native hooks ---
+
+log("[*] TikTok SSL Bypass by 0XSHAK1B");
+
+const targetLibs = ["libttboringssl.so", "libsscronet.so"];
+
+targetLibs.forEach((libName) => {
+    log(`[~] Waiting for ${libName}...`);
+    awaitLibrary(libName).then((mod) => {
+        log(`[+] Loaded ${libName} @ ${mod.base}`);
+        bypassCustomVerify(mod);
+    });
 });
 
+// --- Java hooks ---
 
-Java.perform(function() {
+Java.perform(() => {
+    // Conscrypt TrustManager bypass
     try {
-        var array_list = Java.use("java.util.ArrayList");
-        var ApiClient = Java.use('com.android.org.conscrypt.TrustManagerImpl');
-        if (ApiClient.checkTrustedRecursive) {
-            logger("[*][+] Hooked checkTrustedRecursive")
-            ApiClient.checkTrustedRecursive.implementation = function(a1, a2, a3, a4, a5, a6) {
-                var k = array_list.$new();
-                return k;
-            }
-        } else {
-            logger("[*][-] checkTrustedRecursive not Found")
+        let TrustManagerImpl = Java.use("com.android.org.conscrypt.TrustManagerImpl");
+        if (TrustManagerImpl.checkTrustedRecursive) {
+            TrustManagerImpl.checkTrustedRecursive.implementation = function() {
+                return Java.use("java.util.ArrayList").$new();
+            };
+            log("[+] checkTrustedRecursive patched");
         }
     } catch (e) {
-        logger("[*][-] Failed to hook checkTrustedRecursive")
+        log("[-] checkTrustedRecursive hook failed");
+    }
+
+    // SSLContext.init bypass
+    try {
+        let X509TM = Java.use("javax.net.ssl.X509TrustManager");
+        let SSLCtx = Java.use("javax.net.ssl.SSLContext");
+
+        let EmptyTM = Java.registerClass({
+            name: "com.bypass.sslunpin",
+            implements: [X509TM],
+            methods: {
+                checkClientTrusted(chain, auth) {},
+                checkServerTrusted(chain, auth) {},
+                getAcceptedIssuers() { return []; }
+            }
+        });
+
+        let trustAll = [EmptyTM.$new()];
+        let initMethod = SSLCtx.init.overload(
+            "[Ljavax.net.ssl.KeyManager;",
+            "[Ljavax.net.ssl.TrustManager;",
+            "java.security.SecureRandom"
+        );
+
+        initMethod.implementation = function(km, tm, sr) {
+            initMethod.call(this, km, trustAll, sr);
+        };
+
+        log("[+] SSLContext.init patched");
+    } catch (e) {
+        log("[-] SSLContext.init hook failed");
     }
 });
-
-Java.perform(function() {
-    try {
-        const x509TrustManager = Java.use("javax.net.ssl.X509TrustManager");
-        const sSLContext = Java.use("javax.net.ssl.SSLContext");
-        const TrustManager = Java.registerClass({
-            implements: [x509TrustManager],
-            methods: {
-                checkClientTrusted(chain, authType) {
-                },
-                checkServerTrusted(chain, authType) {
-                },
-                getAcceptedIssuers() {
-                    return [];
-                },
-            },
-            name: "com.leftenter.tiktok",
-        });
-        const TrustManagers = [TrustManager.$new()];
-        const SSLContextInit = sSLContext.init.overload(
-            "[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom");
-        SSLContextInit.implementation = function(keyManager, trustManager, secureRandom) {
-            SSLContextInit.call(this, keyManager, TrustManagers, secureRandom);
-        };
-        logger("[*][+] Hooked SSLContextInit")
-    } catch (e) {
-        logger("[*][-] Failed to hook SSLContextInit")
-    }
-})
